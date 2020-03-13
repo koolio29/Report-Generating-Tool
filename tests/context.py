8ec081d0850fed6a01143f876ea4a165cacc835f
koolio29 <imdad96@gmail.com>
2020-03-13 03:43:05 +0000

    importing all the classes which needs unit testing to the testing context

--- tests/context.py
+++ tests/context.py
@@ -3,4 +3,5 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main import ExamCsvExtractor
+from main import ExamCsvExtractor, DatabaseAccess
+from main import OverallExamStats, OverallMcqStats, OverallEssayStats
